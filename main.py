80f7829e80b9db71b7c221c43b5a66f5e79fc9c6
tukiyo3 <tukiyo3@gmail.com>
2013-07-06 08:01:08 +0900

    fix: template refactoring, add: jquery.js

--- main.py
+++ main.py
@@ -6,55 +6,31 @@ from pyramid.response import Response
 from pyramid.view import view_config
 
 from mako.template import Template
+from mako.lookup import TemplateLookup
 
 import commands
 
 @view_config(route_name='home')
 def home(request):
-    src = "<a href='/execute?command=ls'>execute page</a>"
+    src = "<a href='/execute?command='>execute page</a>"
     return Response(src)
 
 @view_config(route_name='execute')
 def execute(request):
-    import os
-    current_dir = os.getcwd()
+    mak_lookup = TemplateLookup(directories=["templates/"], input_encoding='utf-8')
+    template = mak_lookup.get_template('execute.mak')
     command = request.params['command']
-    template = Template(filename=current_dir+'/templates/execute.mak', input_encoding="utf-8")
-    result = template.render_unicode(result=exe(command))
+    result = template.render_unicode(
+            command_str=command,
+            command_result=run(command)
+        )
     return Response(result)
 
-def exe(command):
+def run(command):
     if command=="":
-        command = "ls"
-    result = commands.getoutput(command)
-    #import dbd; dbd.st_trace
-
-    return form(command) + result + hilighter()
-
-def form(command):
-    src = """
-<html>
-    <head>
-     <meta charset='utf-8'>
-     <script type="text/javascript" src="static/SyntaxHighlighter/scripts/shCore.js"></script>
-     <script type="text/javascript" src="static/SyntaxHighlighter/scripts/shBrushBash.js"></script>
-     <link type="text/css" rel="stylesheet" href="static/SyntaxHighlighter/styles/shCore.css" />
-     <link type="text/css" rel="stylesheet" href="static/SyntaxHighlighter/styles/shThemeDefault.css"/>
-     <style>*{font-size:small;}</style>
-     </head>
-     <body onload=document.forms[0].command.focus();>
-     <form action=/execute method=GET onload=this.focus();>
-     <input name="command" value="%s">
-     <input type="submit" value="execute">
-     </form>
-     <pre class="brush: shell; ruler: true;">""" % command
-    return src.encode('utf-8')
-
-def hilighter():
-    src = """</pre>
-     <script type="text/javascript">SyntaxHighlighter.all();</script>
-    """
-    return src
+        command = "pwd && ls -F"
+    print command
+    return commands.getoutput(command)
 
 if __name__ == '__main__':
     config = Configurator()
--- templates/execute.mak
+++ templates/execute.mak
@@ -1,1 +1,32 @@
-${result.decode('utf-8')}
+<html>
+<head>
+    <meta charset='utf-8'>
+    <script type="text/javascript" src="static/SyntaxHighlighter/scripts/shCore.js"></script>
+    <script type="text/javascript" src="static/SyntaxHighlighter/scripts/shBrushBash.js"></script>
+    <link type="text/css" rel="stylesheet" href="static/SyntaxHighlighter/styles/shCore.css" />
+    <link type="text/css" rel="stylesheet" href="static/SyntaxHighlighter/styles/shThemeDefault.css"/>
+    <script type="text/javascript" src="static/jquery/jquery.js"></script>
+    <style>
+    * {
+        font-size:small;
+    }
+    </style>
+    <script type="text/javascript">
+    <!--
+    $(function(){
+        $("input[name=command]").focus();
+        SyntaxHighlighter.all();
+    });
+    -->
+    </script>
+</head>
+<body>
+
+<form action=/execute method=GET onload=this.focus();>
+<input name="command" value="${command_str.decode('utf-8')}">
+<input type="submit" value="execute">
+</form>
+<pre class="brush: shell; ruler: true;">${command_result.decode('utf-8')}</pre>
+
+</body>
+</html>
